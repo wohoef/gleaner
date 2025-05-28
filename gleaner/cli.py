342bb88657e1cc08f6aa6f2ec4ac2397e3214b9d
Wolter van der Hoef <woltervanderhoef@gmail.com>
2025-05-28 13:54:20 +0200

    cli and scraper skeleton

--- gleaner/cli.py
+++ gleaner/cli.py
@@ -1,2 +1,21 @@
 # Handles arg parsing
 # Entry point for gleaning
+import argparse
+
+from .scraper import Gleaner
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-o", "--output", default="urls.txt")
+    args = parser.parse_args()
+
+    print(f"Gleaning: {args.url}")
+    gleaner = Gleaner(start_url=args.url, output_file=args.output)
+    gleaner.scrape()
+    print(f"Output save to {args.output}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ gleaner/scraper.py
@@ -0,0 +1,4 @@
+class Gleaner:
+    def __init__(self, start_url, output_file):
+        self.start_url = start_url
+        self.output_file = output_file
